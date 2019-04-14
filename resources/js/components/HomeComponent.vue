6f2fb0b1ce0722b017fa77e6ac7765e08214dc63
Camillo <cangelozzi@gmail.com>
2019-04-13 21:37:24 -0400

    Vuetify and Vue Router

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <toolbarcomponent></toolbarcomponent>
+    <footercomponent></footercomponent>
+  </main>
+</template>
+
+<script>
+  import toolbarcomponent from "./layout/ToolbarComponent";
+  import footercomponent from "./layout/FooterComponent.vue";
+  export default {
+    components: {
+      toolbarcomponent,
+      footercomponent
+    }
+  };
+</script>
+
+<style>
+</style>
+
